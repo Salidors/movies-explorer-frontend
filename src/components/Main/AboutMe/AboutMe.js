e3ef56e09b5ff415bcd65df5e5136351a00ffed9
Abramova Nina <abramova.nina.g@gmail.com>
2023-09-27 15:02:15 -0300

    refactor: bem

--- src/components/Main/AboutMe/AboutMe.js
+++ src/components/Main/AboutMe/AboutMe.js
@@ -5,28 +5,24 @@ import userImage from '../../../images/userImage.png';
 export default function AboutMe() {
   return (
     <section className='me'>
-      <div>
-        <h3 className='project-title'>Студент</h3>
-        <div className='line' />
-      </div>
-      <div className='me__private'>
-        <div className='me__description'>
-          <h3 className='me__title'>Виталий</h3>
-          <p className='me__subtitle'>Фронтенд-разработчик, 30 лет</p>
-          <p className='me-about'>
+      <h3 className='me__title me__title-line'>Студент</h3>
+      <div className='me__private-container'>
+        <div className='me__private-description-container'>
+          <h3 className='me__private-name'>Виталий</h3>
+          <p className='me__private-bio'>Фронтенд-разработчик, 30 лет</p>
+          <p className='me__private-about'>
             Я родился и живу в Саратове, закончил факультет экономики СГУ. У
             меня есть жена и дочь. Я люблю слушать музыку, а ещё увлекаюсь
             бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ
             Контур». После того, как прошёл курс по веб-разработке, начал
             заниматься фриланс-заказами и ушёл с постоянной работы.
           </p>
-          <a href='https://github.com/Salidors' className='me__link'>
+          <a href='https://github.com/Salidors' className='link me__private-github'>
             Github
           </a>
         </div>
         <img src={userImage} alt='user' />
       </div>
-      <div className='techs__list'></div>
     </section>
   );
 }
