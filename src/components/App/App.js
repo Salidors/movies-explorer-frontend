87af35a3cc9371bd474abaa9af5be5412babdef8
Abramova Nina <abramova.nina.g@gmail.com>
2023-09-27 15:02:17 -0300

    feat: add app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,22 @@
-import logo from '../../images/logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Layout from '../Layout/Layout';
+import Main from '../Main/Main';
+import PageNotFound from '../PageNotFound/PageNotFound';
 
 function App() {
+  const isAuth = false;
+
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Routes>
+        <Route path='/'>
+          <Route element={<Layout isAuth={isAuth} />}>
+            <Route index element={<Main />} />
+          </Route>
+          <Route path='*' element={<PageNotFound />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
