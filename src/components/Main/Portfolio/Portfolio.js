6f766ef8513ca975112fa4bd20907d04012a0990
Abramova Nina <abramova.nina.g@gmail.com>
2023-09-27 15:02:15 -0300

    refactor: bem

--- src/components/Main/Portfolio/Portfolio.js
+++ src/components/Main/Portfolio/Portfolio.js
@@ -6,22 +6,30 @@ export default function Portfolio() {
   return (
     <section className='portfolio'>
       <h4 className='portfolio__title'>Портфолио</h4>
-      <div className='portfolio__list'>
-        <div className='portfolio__list-item'>
-          <span className='item__text'>Статичный сайт</span>
-          <img src={openArrow} alt='open' />
-        </div>
-        <div className='line-light' />
-        <div className='portfolio__list-item'>
-          <span className='item__text'>Адаптивный сайт</span>
-          <img src={openArrow} alt='open' />
-        </div>
-        <div className='line-light' />
-        <div className='portfolio__list-item'>
-          <span className='item__text'>Одностраничное приложение</span>
-          <img src={openArrow} alt='open' />
-        </div>
-      </div>
+      <nav className='portfolio__nav'>
+        <ul className='portfolio__nav-list'>
+          <li className='portfolio__nav-list-item'>
+            <a href='#' className='link portfolio__links-item'>
+              <span className='item__text'>Статичный сайт</span>
+              <img src={openArrow} alt='open' />
+            </a>
+          </li>
+          <div className='line-light' />
+          <li className='portfolio__nav-list-item'>
+            <a href='#' className='link portfolio__links-item'>
+              <span className='item__text'>Адаптивный сайт</span>
+              <img src={openArrow} alt='open' />
+            </a>
+          </li>
+          <div className='line-light' />
+          <li className='portfolio__nav-list-item'>
+            <a href='#' className='link portfolio__links-item'>
+              <span className='item__text'>Одностраничное приложение</span>
+              <img src={openArrow} alt='open' />
+            </a>
+          </li>
+        </ul>
+      </nav>
     </section>
   );
 }
